83a59530a45b9fc504698202e3d4d5b086cafb2e
Pradeep Panchal <pradeeppanchal0322@gmail.com>
2020-12-06 13:38:14 +0530

    dy

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const https = require("https");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 /////////////
-const https = require("https");
+const fetch=require("node-fetch");
 ////////////////////
 const md5 = require("md5");
 ////////////////
 const ejs = require("ejs");
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 ///////////////////
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://pradeep_22:panchal_22@cluster0.j1m9y.mongodb.net/userDB", {
@@ -22,17 +25,27 @@ mongoose.connect("mongodb+srv://pradeep_22:panchal_22@cluster0.j1m9y.mongodb.net
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  today_list: [String],
-  future_list:[{
-    item:String,
-    date:Date
-  }]
+  today_list: [{
+  item:String,
+  highlight:Number,
+  checked:Number
+  }],
+  future_list: [{
+    item: String,
+    date: Date
+  }],
+  Notes:[{
+    title:String,
+    disc:String
+  }],
+  cityName:String
 });
 const user = mongoose.model('user', userSchema);
 ///////////////////////////////
 app.get("/", function(req, res) {
   res.render("home");
 });
+
 ////////////////////////////////////////////////////
 app.post("/signup", function(req, res) {
   if (req.body.susername === "" || req.body.spassword === "") {
@@ -57,12 +70,17 @@ app.post("/signup", function(req, res) {
           } else {
             const newuser = new user({
               username: req.body.susername,
-              password: md5(req.body.spassword)
+              password: md5(req.body.spassword),
+              today_list:[{item:"item:req.body.item", highlight:0,checked:0}],
+              cityName:"Delhi"
             });
             newuser.save();
-            res.render("todolist", {
-              usr: newuser,
-              day: fun1()
+            getWeather(newuser.cityName).then((data) => {
+                  res.render("today", {
+                    usr: newuser,
+                    day: fun1(),
+                    weather: data
+                  });
             });
           }
         }
@@ -79,9 +97,12 @@ app.post("/login", function(req, res) {
     else {
       if (founduser) {
         if (founduser.password === md5(req.body.lpassword)) {
-          res.render("todolist", {
-            usr: founduser,
-            day: fun1()
+          getWeather(founduser.cityName).then((data) => {
+                res.render("today", {
+                  usr: founduser,
+                  day: fun1(),
+                  weather: data
+                });
           });
         } else {
           res.render("failure", {
@@ -103,35 +124,113 @@ function sortbydate(a,b){
   return dateA >= dateB ? 1 : -1;
 }
 ///////////////////////////////
-app.post("/:username", function(req, res) {
-
+app.post("/:username/today", function(req, res) {
     user.findOne({
     username: req.params.username
   }, function(err, founduser) {
     if (err) console.log(err);
     else {
-      if(req.body.titem){founduser.today_list.push(req.body.titem);}
-      if(req.body.tindex){
-      founduser.today_list.splice(req.body.tindex,1);
+      if(req.body.item){
+        founduser.today_list.push({item:req.body.item,highlight:0,checked:0});
       }
-      if(req.body.fitem){
-        if(new Date(req.body.dt).getTime()>(new Date().getTime())){
-        founduser.future_list.push({item:req.body.fitem,date:req.body.dt});
-        founduser.future_list.sort(sortbydate);}
+      if(req.body.Xindex){
+      founduser.today_list.splice(req.body.Xindex,1);
       }
-      if(req.body.findex){
-        founduser.future_list.splice(req.body.findex, 1);
+      if(req.body.Hindex){
+        var x=founduser.today_list[req.body.Hindex].highlight;
+       founduser.today_list[req.body.Hindex].highlight=(x+1)%2;
+      }
+      if(req.body.Cindex){
+        var x=founduser.today_list[req.body.Cindex].checked;
+       founduser.today_list[req.body.Cindex].checked=(x+1)%2;
+      }
+      if(req.body.cityName){
+      founduser.cityName=req.body.cityName;
       }
       founduser.save();
-      res.render("todolist", {
-        usr: founduser,
-        day: fun1()
-      });
+  getWeather(founduser.cityName).then((data) => {
+        res.render("today", {
+          usr: founduser,
+          day: fun1(),
+          weather: data
+        });
+  });
     }
   });
 });
-///////////////////////////////////
+/////////////////////////////////////////////
+//
+app.post("/:username/upcoming", function(req, res) {
+    user.findOne({
+    username: req.params.username
+  }, function(err, founduser) {
+    if (err) console.log(err);
+    else {
+      if(req.body.item){
+        if(new Date(req.body.date).getTime()>(new Date().getTime())){
+        founduser.future_list.push({item:req.body.item,date:req.body.date});
+        founduser.future_list.sort(sortbydate);}
+      }
+      if(req.body.Xindex){
+        founduser.future_list.splice(req.body.Xindex, 1);
+      }
+      if(req.body.cityName){
+      founduser.cityName=req.body.cityName;
 
+      }
+founduser.save();
+
+/////////////////////////////////////
+  getWeather(founduser.cityName).then((data) => {
+        res.render("upcoming", {
+          usr: founduser,
+          day: fun1(),
+          weather: data
+        });
+  });
+    }
+  });
+});
+///////////////////////////////////////////////////
+
+app.post("/:username/notes", function(req, res) {
+    user.findOne({
+    username: req.params.username
+  }, function(err, founduser) {
+    if (err) console.log(err);
+    else {
+      if(req.body.title){
+        founduser.Notes.push({title:req.body.title,disc:req.body.disc});
+      }
+      if(req.body.Xindex){
+        founduser.Notes.splice(req.body.Xindex, 1);
+      }
+      if(req.body.cityName){
+      founduser.cityName=req.body.cityName;
+      }
+      founduser.save();
+  getWeather(founduser.cityName).then((data) => {
+        res.render("notes", {
+          usr: founduser,
+          day: fun1(),
+          weather: data
+        });
+  });
+    }
+  });
+});
+/////////////////////////////////////
+async function getWeather(city) {
+    const weather = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=8a82db8d6312a6c85216829fe5dd0aa8"
+    );
+    let Wdata = await weather.json();
+    let temp=Wdata.main.temp;
+    let description=Wdata.weather[0].description;
+    let humidity=Wdata.main.humidity;
+    let ans="weather is currently "+description+" with temperature "+temp+" Celcius and humidity "+humidity+ "% at ";
+    return ans;
+}
 ////////////////////////////////
 function fun1() {
   var day;
